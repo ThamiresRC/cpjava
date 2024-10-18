1bfea1e93fbf41fd95473ef5dfeae5f961b71c0e
GustavoSMelo <gsmelo2804@gmail.com>
2024-10-18 18:23:53 -0300

    sla2

--- src/main/java/br/com/fiap/dao/ClienteDAO.java
+++ src/main/java/br/com/fiap/dao/ClienteDAO.java
@@ -5,7 +5,7 @@ import br.com.fiap.model.Cliente;
 public interface ClienteDAO {
 
         void salvar(Cliente cliente);
-//        Cliente buscarPorId(int id);
+        Cliente buscarPorId(int id);
 //        void atualizar(Cliente cliente);
 //        void deletar(int id);
 }
